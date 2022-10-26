d9511c6ef1ca45b981649df69fa80c8560f32b52
Darisha <dashasv03@gmail.com>
2022-10-26 21:11:40 +0300

    init commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.calculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+
+    private var res: Float? = null;
+
+    private var firstNumber: Float? = null;
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun getResultFieldValue(): Float  {
+        var input: TextInputEditText = findViewById(R.id.resultsET)
+
+        return input.text.toString().toFloat();
+    }
+
+    fun clearInput() {
+        var input: TextInputEditText = findViewById(R.id.resultsET)
+        input.setText("");
+    }
+
+    fun clear(view: View) {
+        this.res = null;
+        clearInput()
+    }
+
+    fun add(view: View) {
+        var secondNumber = getResultFieldValue();
+
+        this.res = this.firstNumber!! + secondNumber;
+        clearInput();
+    }
+
+    fun subtract(view: View) {
+        var secondNumber = getResultFieldValue();
+
+        this.res = this.firstNumber!! - secondNumber;
+        clearInput();
+    }
+
+    fun multiply(view: View) {
+        var secondNumber = getResultFieldValue();
+
+        this.res = this.firstNumber!! * secondNumber;
+        clearInput();
+    }
+
+    fun divide(view: View) {
+        var secondNumber = getResultFieldValue();
+
+        this.res = this.firstNumber!! / secondNumber;
+        clearInput();
+    }
+
+    fun count(view: View) {
+
+        val intent = Intent(this, ResultActivity::class.java);
+
+        var extraResult = ""
+
+        if (this.res != null) {
+            extraResult = this.res.toString();
+        }
+
+        intent.putExtra("result", extraResult)
+        startActivity(intent)
+
+        this.res = null
+        clearInput();
+    }
+
+    fun and(view: View) {
+        this.firstNumber = getResultFieldValue()
+
+        clearInput();
+    }
+}
